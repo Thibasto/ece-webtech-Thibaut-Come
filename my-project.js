7a961d9a6046d06d0362b6c82442a8698ce1cc73
Thibasto <thibaut.dastorg@edu.ece.fr>
2022-09-27 11:54:40 +0200

    Update my-project.js

--- my-project.js
+++ my-project.js
@@ -1,11 +1,21 @@
 // Import a module
 const http = require('http')
 
-// Declare an http server
-const serverHandle = function (req, res) 
-{
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
+const content = '<!DOCTYPE html>' +
+'<html>' +
+'    <head>' +
+'        <meta charset="utf-8" />' +
+'        <title>ECE AST</title>' +
+'    </head>' + 
+'    <body>' +
+'       <p>Hello World!</p>' +
+'    </body>' +
+'</html>'
+
+const serverHandle = function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write(content);
+  res.end();
 }
 
 const server = http.createServer(serverHandle);
