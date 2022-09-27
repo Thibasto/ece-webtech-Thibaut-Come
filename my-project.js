203b001825929ceb77aa086e50836f07aa6bc8e6
Thibasto <thibaut.dastorg@edu.ece.fr>
2022-09-27 12:06:22 +0200

    Update my-project.js

--- my-project.js
+++ my-project.js
@@ -15,13 +15,20 @@ const content = '<!DOCTYPE html>' +
 '</html>'
 
 const serverHandle = function (req, res) {
-const path = url.parse(req.url).pathname;
-  console.log(path);
-const queryParams = qs.parse(url.parse(req.url).query);
-  console.log(queryParams);
+
+const route = url.parse(req.url)
+  const path = route.pathname 
+  const params = qs.parse(route.query)
+
 
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write(content);
+
+  if (path === '/hello' && 'name' in params) {
+    res.write('Hello ' + params['name'])
+  } else {
+    res.write('Hello anonymous')
+  }
+
   res.end();
 }
 
