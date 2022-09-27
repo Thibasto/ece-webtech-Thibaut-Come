182b4f765ba3d7c25e14538d7d8906a64082c07c
Thibasto <thibaut.dastorg@edu.ece.fr>
2022-09-27 11:52:46 +0200

    Update my-project.js

--- my-project.js
+++ my-project.js
@@ -2,13 +2,11 @@
 const http = require('http')
 
 // Declare an http server
-http.createServer(function (req, res) {
-
-  // Write a response header
+const serverHandle = function (req, res) 
+{
   res.writeHead(200, {'Content-Type': 'text/plain'});
-
-  // Write a response content
   res.end('Hello World\n');
+}
 
-// Start the server
-}).listen(8080)
+const server = http.createServer(serverHandle);
+server.listen(8080)
