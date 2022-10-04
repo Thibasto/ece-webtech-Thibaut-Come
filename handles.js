548ac16edb5a246f153ae0dc23b2f7e1fdc91291
Thibasto <thibaut.dastorg@edu.ece.fr>
2022-10-04 09:51:35 +0200

    5

--- handles.js
+++ handles.js
@@ -5,6 +5,44 @@ const handles = require('./handles')
 const url = require('url')
 const qs = require('querystring')
 
+const contentThibaut=
+'<!DOCTYPE html>' +
+'<html>' +
+'    <head>' +
+'        <meta charset="utf-8" />' +
+'        <title>LAB2</title>' +
+'    </head>' +
+'    <body>' +
+'       <h1>Thibaut d Astorg</h1>' +
+'       <h2>ING4 Information System GRP3</h2>'+
+'    </body>' +
+'</html>'
+
+const contentCome=
+'<!DOCTYPE html>' +
+'<html>' +
+'    <head>' +
+'        <meta charset="utf-8" />' +
+'        <title>LAB2</title>' +
+'    </head>' +
+'    <body>' +
+'       <h1>Come Chappuis</h1>' +
+'       <h2>ING4 Information System GRP3</h2>'+
+'    </body>' +
+'</html>'
+
+const contentError404 =
+'<!DOCTYPE html>' +
+'<html>' +
+'    <head>' +
+'        <meta charset="utf-8" />' +
+'        <title>404</title>' +
+'    </head>' +
+'    <body>' +
+'       <h2>ERROR 404 PAGE NOT FOUND</h2>'+
+'    </body>' +
+'</html>'
+
 module.exports = {
      serverHandle : function (req, res) {
 
@@ -14,13 +52,31 @@ module.exports = {
         
         
           res.writeHead(200, {'Content-Type': 'text/html'});
-        
-          if (path === '/hello' && 'name' in params) {
-            res.write('Hello ' + params['name'])
-          } else {
-            res.write('Hello anonymous')
+
+
+          if (path === '/hello' && 'name' in params) 
+          {
+            if(params['name'] == 'Thibaut')
+            {
+              res.writeHead(200, {'Content-Type': 'text/html'});
+              res.write(contentThibaut);
+            }
+            if(params['name'] == 'Come')
+            {
+              res.writeHead(200, {'Content-Type': 'text/html'});
+              res.write(contentCome);
+            }
+            else
+            {
+              res.writeHead(200, {'Content-Type': 'text/plain'});
+              res.write('Hello ' + params['name']);
+            }
           }
-        
+          else 
+          {
+          res.writeHead(404, {'Content-Type': 'text/html'});
+          res.write(contentError404);
+          }
           res.end();
         }
   }
--- index.js
+++ index.js
@@ -1,20 +1,6 @@
 // Import a module
 const http = require('http')
 const handles = require('./handles')
-const url = require('url')
-const qs = require('querystring')
-
-
-const content = '<!DOCTYPE html>' +
-'<html>' +
-'    <head>' +
-'        <meta charset="utf-8" />' +
-'        <title>ECE AST</title>' +
-'    </head>' + 
-'    <body>' +
-'       <p>Hello World!</p>' +
-'    </body>' +
-'</html>'
 
 
 
